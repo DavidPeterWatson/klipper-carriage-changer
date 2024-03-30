ed3c43eced43fc7884ea33f137c9e8dd5c76032f
David Watson <davidpeterwatson@gmail.com>
2024-03-30 13:24:06 +1300

    test

--- carriage.py
+++ carriage.py
@@ -8,6 +8,7 @@ class Carriage:
         self.offset_y = config.get('offset_y')
         self.offset_z = config.get('offset_z')
         self.printer.add_object('carriage ' + self.name, self)
+        test = self.printer['carriage ' + self.name]
         # gcode_macro = self.printer.load_object(config, 'gcode_macro')
         # self.after_load_template = gcode_macro.load_template(config, 'after_load_gcode')
         # self.after_unload_template = gcode_macro.load_template(config, 'after_unload_gcode', '')
