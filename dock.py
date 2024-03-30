33b95d82d77bcaa379fb1f5568f68555c7d25af5
David Watson <davidpeterwatson@gmail.com>
2024-03-30 13:54:38 +1300

    add_object change

--- dock.py
+++ dock.py
@@ -9,7 +9,7 @@ class Dock:
         self.load_x = config.get('load_xd')
         self.loading_speed = config.get('loading_speed')
         self.loading_pause = config.get('loading_pause')
-        self.printer.add_object('dock ' + self.name, self)
+        self.printer.add_object('dock-' + self.name, self)
 
 
 def load_config_prefix(config):
