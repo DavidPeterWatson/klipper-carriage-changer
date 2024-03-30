67391ab4ab8caaa22e51588e68a9e3f23687c4b0
David Watson <davidpeterwatson@gmail.com>
2024-03-30 15:01:44 +1300

    float

--- dock.py
+++ dock.py
@@ -3,10 +3,10 @@ class Dock:
         self.printer = config.get_printer()
         self.name = config.get_name().split(' ')[-1]
         self.location = config.get('location')
-        self.safe_y = config.get('safe_y')
-        self.safe_zd = config.get('safe_zd')
-        self.load_yd = config.get('load_yd')
-        self.load_xd = config.get('load_xd')
+        self.safe_y = float(config.get('safe_y'))
+        self.safe_zd = float(config.get('safe_zd'))
+        self.load_yd = float(config.get('load_yd'))
+        self.load_xd = float(config.get('load_xd'))
         self.loading_speed = float(config.get('loading_speed')) * 60
         self.loading_pause = config.get('loading_pause')
         self.printer.add_object('dock ' + self.name, self)
