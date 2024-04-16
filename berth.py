9fc578d66910fdd716e163c99173d501ef41cda0
David Watson <davidpeterwatson@gmail.com>
2024-04-16 18:17:28 +1200

    self.dock

--- berth.py
+++ berth.py
@@ -2,7 +2,7 @@ class Berth:
     def __init__(self, config):
         self.printer = config.get_printer()
         self.name = config.get_name().split(' ')[-1]
-        self.dock_name = config.get('dock')
+        self.dock = config.get('dock')
         self.x_pos = config.get('x_pos')
         self.printer.add_object('berth ' + self.name, self)
 
