b8544e2cb7a276a8ab9eeb494893949dfb65f01a
David Watson <davidpeterwatson@gmail.com>
2024-04-16 13:50:01 +1200

    carriage_movement

--- carriage_changer.py
+++ carriage_changer.py
@@ -1,6 +1,6 @@
 import os
 
-class CarraigeChanger:
+class CarriageChanger:
     def __init__(self, config):
         self.printer = config.get_printer()
 
@@ -9,11 +9,11 @@ class CarraigeChanger:
         dirname = os.path.dirname(os.path.realpath(__file__))
         filename = os.path.join(dirname, 'carriage_movement.cfg')
         try:
-            cariage_movement = pconfig.read_config(filename)
+            carriage_movement = pconfig.read_config(filename)
         except Exception:
             raise config.error("Cannot load config '%s'" % (filename,))
-        for section in cariage_movement.get_prefix_sections(''):
-            self.printer.load_object(cariage_movement, section.get_name())
+        for section in carriage_movement.get_prefix_sections(''):
+            self.printer.load_object(carriage_movement, section.get_name())
 
 def load_config_prefix(config):
-    return CarraigeChanger(config)
+    return CarriageChanger(config)
