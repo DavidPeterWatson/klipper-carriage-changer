497de5998cdc2deed2324ab9dfc8392e289163ae
David Watson <davidpeterwatson@gmail.com>
2024-04-22 18:03:29 +1200

    safe_z

--- dock.py
+++ dock.py
@@ -4,7 +4,6 @@ class Dock:
         self.name = config.get_name().split(' ')[-1]
         self.location = config.get('location')
         self.safe_y = float(config.get('safe_y') or 0)
-        self.safe_z = float(config.get('safe_z') or 20)
         self.load_yd = float(config.get('load_yd') or 0)
         self.load_xd = float(config.get('load_xd') or 0)
         self.align_speed = float(config.get('align_speed')) * 60
