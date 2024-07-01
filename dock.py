a5c6ae3fde79fe4d229ee7e17eea3ec5c570559e
David Watson <davidpeterwatson@gmail.com>
2024-07-01 21:17:58 +1200

    Change safety checks

--- dock.py
+++ dock.py
@@ -6,6 +6,7 @@ class Dock:
         self.safe_y = float(config.get('safe_y') or 0)
         self.load_yd = float(config.get('load_yd') or 0)
         self.engage_xd = float(config.get('engage_xd') or 0)
+        self.safe_zone_button = config.get('safe_zone_button')
         self.printer.add_object('dock ' + self.name, self)
 
 
